fce5841b86dbd63492afc7fa6ab1db044825aa1e
qhk895 <79901178+qhk895@users.noreply.github.com>
2022-09-07 11:55:09 +0200

    Update test.py

--- Ardiuno files/test.py
+++ Ardiuno files/test.py
@@ -3,26 +3,26 @@ from time import sleep
 
 arlo = robot.Robot()
 
-print(arlo.go_diff(64,64,1,1))
-sleep(1.5)
+print(arlo.go_diff(66,64,1,1))
+sleep(2)
 
 print(arlo.go_diff(64,64,1,0))
 sleep(0.7)
 
-print(arlo.go_diff(64,64,1,1))
-sleep(1.5)
+print(arlo.go_diff(66,64,1,1))
+sleep(2)
 
 print(arlo.go_diff(64,64,1,0))
 sleep(0.7)
 
-print(arlo.go_diff(64,64,1,1))
-sleep(1.5)
+print(arlo.go_diff(66,64,1,1))
+sleep(2)
 
 print(arlo.go_diff(64,64,1,0))
 sleep(0.7)
 
-print(arlo.go_diff(64,64,1,1))
-sleep(1.5)
+print(arlo.go_diff(66,64,1,1))
+sleep(2)
 
 print(arlo.go_diff(64,64,1,0))
 sleep(0.7)
