25589b9ccb7c0bbe845e89b7487d8a5fa76f41ae
qhk895 <79901178+qhk895@users.noreply.github.com>
2022-09-07 12:39:15 +0200

    Update test.py

--- Ardiuno files/test.py
+++ Ardiuno files/test.py
@@ -3,29 +3,52 @@ from time import sleep
 
 arlo = robot.Robot()
 
+# 1 meter
 print(arlo.go_diff(66,64,1,1))
 sleep(2)
-
-print(arlo.go_diff(64,64,1,0))
-sleep(0.7)
-
-print(arlo.go_diff(66,64,1,1))
-sleep(2)
-
-print(arlo.go_diff(64,64,1,0))
-sleep(0.7)
-
-print(arlo.go_diff(66,64,1,1))
-sleep(2)
-
-print(arlo.go_diff(64,64,1,0))
-sleep(0.7)
-
-print(arlo.go_diff(66,64,1,1))
-sleep(2)
-
-print(arlo.go_diff(64,64,1,0))
-sleep(0.7)
 print(arlo.stop)
 
-print("finish")
+# Square
+#print(arlo.go_diff(66,64,1,1))
+#sleep(2)
+
+#print(arlo.go_diff(64,64,1,0))
+#sleep(0.7)
+
+#print(arlo.go_diff(66,64,1,1))
+#sleep(2)
+
+#print(arlo.go_diff(64,64,1,0))
+#sleep(0.7)
+
+#print(arlo.go_diff(66,64,1,1))
+#sleep(2)
+
+#print(arlo.go_diff(64,64,1,0))
+#sleep(0.7)
+
+#print(arlo.go_diff(66,64,1,1))
+#sleep(2)
+
+#print(arlo.go_diff(64,64,1,0))
+#sleep(0.7)
+#print(arlo.stop)
+
+#print("finish")
+
+# Continuous motion (8)
+#print(arlo.go_diff(66,64,1,1))
+#sleep(2)
+
+#print(arlo.go_diff(64,0,1,1))
+#sleep(3.3)
+
+#print(arlo.go_diff(66,64,1,1))
+#sleep(2)
+
+#print(arlo.go_diff(0,64,1,1))
+#sleep(3.3)
+
+#print(arlo.go_diff(64,0,1,1))
+#sleep(3.3)
+#print(arlo.stop)
