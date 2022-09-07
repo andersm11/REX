30ce6f2702741a4626e13e8d47d7afa58887867b
qhk895 <79901178+qhk895@users.noreply.github.com>
2022-09-07 12:51:59 +0200

    Update test.py

--- Ardiuno files/test.py
+++ Ardiuno files/test.py
@@ -4,9 +4,9 @@ from time import sleep
 arlo = robot.Robot()
 
 # 1 meter
-print(arlo.go_diff(49,49,1,1))
-sleep(3)
-print(arlo.stop)
+#print(arlo.go_diff(49,49,1,1))
+#sleep(3)
+#print(arlo.stop)
 
 # Square
 #print(arlo.go_diff(66,64,1,1))
@@ -37,18 +37,18 @@ print(arlo.stop)
 #print("finish")
 
 # Continuous motion (8)
-#print(arlo.go_diff(66,64,1,1))
-#sleep(2)
+print(arlo.go_diff(66,64,1,1))
+sleep(2)
 
-#print(arlo.go_diff(64,0,1,1))
-#sleep(3.3)
+print(arlo.go_diff(64,0,1,1))
+sleep(3.3)
 
-#print(arlo.go_diff(66,64,1,1))
-#sleep(2)
+print(arlo.go_diff(66,64,1,1))
+sleep(2)
 
-#print(arlo.go_diff(0,64,1,1))
-#sleep(3.3)
+print(arlo.go_diff(0,64,1,1))
+sleep(3.3)
 
-#print(arlo.go_diff(64,0,1,1))
-#sleep(3.3)
-#print(arlo.stop)
+print(arlo.go_diff(64,0,1,1))
+sleep(3.3)
+print(arlo.stop)
