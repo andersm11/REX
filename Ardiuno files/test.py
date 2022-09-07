603a0a5dffbc54ba6cd461d5ef493cebd5a372df
RuneBrix <rune.brix0611@gmail.com>
2022-09-07 15:23:16 +0200

    Update test.py

--- Ardiuno files/test.py
+++ Ardiuno files/test.py
@@ -12,34 +12,40 @@ arlo = robot.Robot()
 #print(arlo.go_diff(0,30,1,1))
 #sleep(0.1)
 #print(arlo.stop)
+for i in range(3):
+    print(arlo.go_diff(66,64,1,1))
+    sleep(2)
+
+    print(arlo.go_diff(64,64,1,0))
+    sleep(0.7)
 
 # Square
-print(arlo.go_diff(66,64,1,1))
-sleep(2)
+# print(arlo.go_diff(66,64,1,1))
+# sleep(2)
 
-print(arlo.go_diff(64,64,1,0))
-sleep(0.7)
+# print(arlo.go_diff(64,64,1,0))
+# sleep(0.7)
 
-print(arlo.go_diff(66,64,1,1))
-sleep(2)
+# print(arlo.go_diff(66,64,1,1))
+# sleep(2)
 
-print(arlo.go_diff(64,64,1,0))
-sleep(0.7)
+# print(arlo.go_diff(64,64,1,0))
+# sleep(0.7)
 
-print(arlo.go_diff(66,64,1,1))
-sleep(2)
+# print(arlo.go_diff(66,64,1,1))
+# sleep(2)
 
-print(arlo.go_diff(64,64,1,0))
-sleep(0.7)
+# print(arlo.go_diff(64,64,1,0))
+# sleep(0.7)
 
-print(arlo.go_diff(66,64,1,1))
-sleep(2)
+# print(arlo.go_diff(66,64,1,1))
+# sleep(2)
 
-print(arlo.go_diff(64,64,1,0))
-sleep(0.7)
-print(arlo.stop)
+# print(arlo.go_diff(64,64,1,0))
+# sleep(0.7)
+# print(arlo.stop)
 
-print("finish")
+# print("finish")
 
 # Continuous motion (8)
 
