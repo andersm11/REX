3b0d2bd93c21acc9087539ad2ff0d2b5bdc4852b
Mehmetunsal2001 <mehmet.unsal@live.dk>
2022-09-12 15:37:56 +0200

    m

--- Ardiuno files/test.py
+++ Ardiuno files/test.py
@@ -14,15 +14,15 @@ print(arlo.go_diff(40,40,1,1))
 #arlo.stop()
 #
 dist = True
-while True:
+while dist:
     a = arlo.read_front_ping_sensor()
     b = arlo.read_back_ping_sensor()
     c = arlo.read_right_ping_sensor()
     d = arlo.read_left_ping_sensor()
     sleep(0.2)
     print("a =", a, ", b=", b, ", c=", c, ", d=",d)
-    if a > 300 or b > 300 or c > 300 or d > 300:
-        False
+    if a < 300 or b < 300 or c < 300 or d < 300:
+        dist = False
 
 arlo.stop()
 
