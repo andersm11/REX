a1fb58722824f4253fc6a0490d570af4f0a0fc63
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-09-21 14:40:07 +0200

    Create testa.py

---
+++ Ardiuno files/Examples/opencv2-python/gstreamer/Example1/testa.py
@@ -0,0 +1,47 @@
+from symbol import parameters
+import cv2 # Import the OpenCV library
+import cv2.aruco as aruco
+
+
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+arucoParams = cv2.aruco.DetectorParameters_create()
+
+def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=30):
+    """Utility function for setting parameters for the gstreamer camera pipeline"""
+    return (
+        "libcamerasrc !"
+        "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "videoconvert ! "
+        "appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+        )
+    )
+
+cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
+
+
+if not cam.isOpened(): # Error
+    print("Could not open camera")
+    exit(-1)
+
+WIN_RF = "Example 1"
+cv2.namedWindow(WIN_RF)
+cv2.moveWindow(WIN_RF, 100, 100)
+
+
+while cv2.waitKey(32) == -1: # Wait for a key pressed event
+    retval, frameReference = cam.read() # Read frame
+    
+    if not retval: # Error
+        print(" < < <  Game over!  > > > ")
+        exit(-1)
+    
+    (corners, ids, rejected) = aruco.detectMarker(frameReference,aruco_dict,parameters=arucoParams)
+    print(ids, "\n")
+    
+    # Show frames
+    # cv2.imshow(WIN_RF, frameReference)
+
