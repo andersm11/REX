222fece21bcd188f9058504fd5076fc9ab7a2dec
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-09-28 17:20:15 +0200

    Update TILRUNE.py

--- Ardiuno files/TILRUNE.py
+++ Ardiuno files/TILRUNE.py
@@ -85,13 +85,13 @@ def CheckID(id):
 def Turn(angle):
     if angle <= 0:
         arlo.go_diff(30,30,0,1)
-        print(0.0005*abs(angle))
-        sleep(0.0005*abs(angle))
+        print(0.019*abs(angle))
+        sleep(0.019*abs(angle))
         arlo.stop()
     else:
         arlo.go_diff(30,30,1,0)
-        print(0.0005*abs(angle))
-        sleep(0.0005*abs(angle))
+        print(0.019*abs(angle))
+        sleep(0.019*abs(angle))
         arlo.stop()
 
 
