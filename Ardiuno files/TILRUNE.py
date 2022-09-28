777006432cca5722c5f968d9d6cf09c13e8076ad
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-09-28 16:42:40 +0200

    Update TILRUNE.py

--- Ardiuno files/TILRUNE.py
+++ Ardiuno files/TILRUNE.py
@@ -77,7 +77,7 @@ x = np.array([1,0,0],dtype=float)
 box_id = 3
 def CheckID(id):
     if id is not None:
-        if id.any() == box_id:
+        if id[0] == box_id:
             return True
         else:
             return False
