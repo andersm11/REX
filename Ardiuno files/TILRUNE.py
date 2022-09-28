7538597daf783b44dde774693758e1e68a8b1b45
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-09-28 16:45:32 +0200

    Update TILRUNE.py

--- Ardiuno files/TILRUNE.py
+++ Ardiuno files/TILRUNE.py
@@ -133,6 +133,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
             print(length)
             sleep(0.028*(length-20))
             arlo.stop()
+            exit(1)
     else:
         arlo.go_diff(30,30,1,0)
         sleep(0.5)
