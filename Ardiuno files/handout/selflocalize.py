e14bd8807b8fc562a2f81699931de5aaef23df32
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-10-05 17:09:51 +0200

    Update selflocalize.py

--- Ardiuno files/handout/selflocalize.py
+++ Ardiuno files/handout/selflocalize.py
@@ -63,6 +63,7 @@ def normalize_weights(particles):
     for p1 in particles:
         w_sum += p1.getWeight()
     print("w_sum:",w_sum,"\n")
+    print("norm:", p2.getWeight()/(w_sum),"\n")
     for p2 in particles:
         norm_weight = p2.getWeight()/(w_sum)
         p2.setWeight(norm_weight)
