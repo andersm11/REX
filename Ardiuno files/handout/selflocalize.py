16b1e85b66ba010e049319c4b7fa7944697593fb
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-10-05 15:50:27 +0200

    Update selflocalize.py

--- Ardiuno files/handout/selflocalize.py
+++ Ardiuno files/handout/selflocalize.py
@@ -54,9 +54,10 @@ def compute_weights(landmarkIDs,landmark_d, landmark_a ,old_particles):
     for op in old_particles:
         weight = 1
         for i in range(len(landmarkIDs)):
-            d = distance(landmarks[landmarkIDs[i]][0],landmarks[landmarkIDs[i]][1],op.getX(),op.getY()) #hypo distance
-            dm = landmark_d[i]
-            weight = weight * gaussian_pdf_distance(d,dm,0.2)*gaussian_pdf_angle(landmark_a[i],landmarks[landmarkIDs[i]][0],landmarks[landmarkIDs[i]][1],op.getX(),op.getY(),op.getTheta(),0.2)
+            if landmarkIDs[i] in landmarks:
+                d = distance(landmarks[landmarkIDs[i]][0],landmarks[landmarkIDs[i]][1],op.getX(),op.getY()) #hypo distance
+                dm = landmark_d[i]
+                weight = weight * gaussian_pdf_distance(d,dm,0.2)*gaussian_pdf_angle(landmark_a[i],landmarks[landmarkIDs[i]][0],landmarks[landmarkIDs[i]][1],op.getX(),op.getY(),op.getTheta(),0.2)
         pweights.append((old_particles,weight))
     return pweights
 
