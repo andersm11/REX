8faedc870ca36076dad8207c5a9933c11cc0a110
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-10-10 15:34:37 +0200

    Update selflocalize.py

--- Ardiuno files/handout/selflocalize.py
+++ Ardiuno files/handout/selflocalize.py
@@ -79,7 +79,7 @@ def resample_gaussian(particles):
     weights = []
     for p in particles:
         weights.append(p.getWeight())
-    print(sum(weights))
+    print("sum:",sum(weights))
     resamples = np.random.choice(particles,1000,p=weights,replace=True)
     return resamples
 
