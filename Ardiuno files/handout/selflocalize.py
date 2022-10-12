10b46e3a2cc44471fa8768617201b2fb03db32d4
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-10-12 10:32:24 +0200

    Update selflocalize.py

--- Ardiuno files/handout/selflocalize.py
+++ Ardiuno files/handout/selflocalize.py
@@ -343,7 +343,7 @@ try:
                 resamples = resample_gaussian(particles)
                 for i in range(len(resamples)):
                     particles[i] = copy.deepcopy(resamples[i])
-                #particles = add_uncertainty(particles,0.2,0.2)
+                particles = add_uncertainty(particles,0.2,0.2)
                 # Draw detected objects
             cam.draw_aruco_objects(colour)
         else:
