582e4b57a4b5a5d66994888166576978402b39b9
RuneBrix <rune.brix0611@gmail.com>
2022-10-26 15:01:47 +0200

    Update rally.py

--- Ardiuno files/handout/rally.py
+++ Ardiuno files/handout/rally.py
@@ -1,97 +1,15 @@
-try:
-    import robot
-    onRobot = True
-except ImportError:
-    print("selflocalize.py: robot module not present - forcing not running on Arlo!")
-    onRobot = False
-
-import cv2
-import Ex3
-
-targets = [1, 2, 3, 4, 1]
-states = [0,1,2,3,4]
-class  Landmark():
-    def __init__(self, state, landmarks):
-        self.state = state
-        self.landmarks = landmarks
-
-    
-    def find(self, landmark):
-        #kode til at checke om kamera kan se aruco kode
-        # det her går jo slet ikke.. I know
-        for i in range(self.landmarks):
-            if landmark == self.landmarks[i]:
-                return True
-        else: 
-            return False
-    
-    def nextLandmark(self):
-        landmarks = landmarks[i+1]
-        state = state[i+1]
-
-    def lastLandmark(self):
-        if self.state[i] == 4:
-            return True
-        else: 
-            return False
-
-landmark = Landmark(0,1)
-
-def searchtarget(targetbox):
-    Ex3.search_and_find
-    else:
-        changeposition()
-
-def changeposition():
-    while arlo.read_front_ping_sensor(self) > 200 and arlo.read_left_ping_sensor(self) > 100 and arlo.read_right_ping_sensor(self) > 100:
-        #kør 30 cm frem
-        searchtarget(targetbox)
-    opstacleavoid()
-
-def drivetotarget(targetbox):
-    while arlo.read_front_ping_sensor(self) > 200 and arlo.read_left_ping_sensor(self) > 100 and arlo.read_right_ping_sensor(self) > 100:
-        #vend mod targetbox
-        #kør mod targetbox
-        if targetbox.last_targetbox == True:
-            arlo.stop
-        elif dist(targetbox) <= 400:
-            targetbox.nexttargetbox
-            searchtarget(targetbox)
-    opstacleavoid
-    searchtarget(targetbox)
-
-def opstacleavoid():
-    if arlo.read_front_ping_sensor(self) > 200:
-        searchtarget(targetbox)
-    else:
-        while arlo.read_front_ping_sensor() < 200:
-            if arlo.read_left_ping_sensor() > 100:
-                #drej 10 grader til venstre
-            elif arlo.read_right_ping_sensor() > 100:
-                #drej 10 grader mod højre
-            else
-                #kør 10 cm tilbage
-                #drej 10 grader til venstre
-        #kør 10 cm frem
-        obstacleavoid()
-
-
-    
-
-
-searchtarget(targetbox)
-
 from re import I, search
 from turtle import right
 import robot
 from time import sleep 
 import selflocalize
 import particle
-
+import cv2
+import Ex3
 arlo = robot.Robot()
 
-############ LANDMARK CLASS ##############
-# Er det her nødtvendigt? 
+landmarks = [2,4]
+states = [0,1]
 class  Landmark():
     def __init__(self, state, landmarks):
         self.state = state
@@ -112,15 +30,93 @@ class  Landmark():
         state = state[i+1]
 
     def lastLandmark(self):
-        if self.state[i] == 3:
+        if self.state[i] == 1:
             return True
         else: 
             return False
 
-############ LANDMARK CLASS ##############
-#################  END  ##################
+landmark = Landmark(states[0],landmarks[0])
 
+#def searchtarget(landmark):
+#    while arlo.read_front_ping_sensor(self) > 200 and arlo.read_left_ping_sensor(self) > 100 and arlo.read_right_ping_sensor(self) > 100:
+#        if Ex3.CheckID(landmark.landmarks) == True:
+#            Ex3.search_and_find
+#        else:
+#            changeposition()
+#            searchtarget(landmark)
+#    obstacleavoid()
 
+def changeposition():
+    while arlo.read_front_ping_sensor() > 200 and arlo.read_left_ping_sensor() > 100 and arlo.read_right_ping_sensor() > 100:
+        arlo.go_diff(40,40,1,1)
+        sleep(1)
+        Ex3.search_and_find(landmark)
+    obstacleavoid()
+
+#def drivetotarget(landmark):
+#    while arlo.read_front_ping_sensor(self) > 200 and arlo.read_left_ping_sensor(self) > 100 and arlo.read_right_ping_sensor(self) > 100:
+#        #vend mod targetbox
+#        #kør mod targetbox
+#        if landmark.lastLandmark == True:
+#            arlo.stop
+#        elif dist(landmark) <= 400:
+#            landmark.nextLandmark
+#            searchtarget(landmark)
+#    obstacleavoid
+#    searchtarget(targetbox)
+
+def obstacleavoid():
+    if arlo.read_front_ping_sensor() > 200:
+        Ex3.search_and_find(landmark)
+    else:
+        while arlo.read_front_ping_sensor() < 200:
+            if arlo.read_left_ping_sensor() > 200:
+                arlo.go_diff(30,30,0,1)
+                sleep(0.3)
+            elif arlo.read_right_ping_sensor() > 200:
+                arlo.go_diff(30,30,1,0)
+                sleep(0.3)
+            else:
+                arlo.go_diff(30,30,0,0)
+                sleep(0.5)
+                arlo.go_diff(30,30,0,1)
+                sleep(0.3)
+                arlo.stop
+        obstacleavoid()
+
+Ex3.search_and_find()
+
+############# LANDMARK CLASS ##############
+## Er det her nødtvendigt? 
+#class  Landmark():
+#    def __init__(self, state, landmarks):
+#        self.state = state
+#        self.landmarks = landmarks
+#
+#    
+#    def find(self, landmark):
+#        #kode til at checke om kamera kan se aruco kode
+#        # det her går jo slet ikke.. I know
+#        for i in range(self.landmarks):
+#            if landmark == self.landmarks[i]:
+#                return True
+#        else: 
+#            return False
+#    
+#    def nextLandmark(self):
+#        landmarks = landmarks[i+1]
+#        state = state[i+1]
+#
+#    def lastLandmark(self):
+#        if self.state[i] == 3:
+#            return True
+#        else: 
+#            return False
+#
+############# LANDMARK CLASS ##############
+##################  END  ##################
+#
+#
 
 ############ Particle filter #############
 
