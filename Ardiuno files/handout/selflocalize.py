9252e82944a17c047af0e9ca78ab920ed3b25514
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-10-26 10:14:33 +0200

    Update selflocalize.py

--- Ardiuno files/handout/selflocalize.py
+++ Ardiuno files/handout/selflocalize.py
@@ -133,10 +133,10 @@ CBLACK = (0, 0, 0)
 
 # Landmarks.
 # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [1, 2]
+landmarkIDs = [2, 1]
 landmarks = {
-    1: (0.0, 0.0),  # Coordinates for landmark 1
-    2: (300.0, 0.0)  # Coordinates for landmark 2
+    2: (0.0, 0.0),  # Coordinates for landmark 1
+    1: (300.0, 0.0)  # Coordinates for landmark 2
 }
 landmark_colors = [CRED, CGREEN] # Colors used when drawing the landmarks
 
