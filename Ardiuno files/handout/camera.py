adfe857969d84d77a95985333e263b3548aa1932
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-11-02 15:57:49 +0100

    Update camera.py

--- Ardiuno files/handout/camera.py
+++ Ardiuno files/handout/camera.py
@@ -36,11 +36,11 @@ def gstreamer_pipeline(capture_width=1280, capture_height=720, framerate=30):
     """Utility function for setting parameters for the gstreamer camera pipeline"""
     return (
         "libcamerasrc !"
-        "videobox autocrop=true !"
+        "videobox autocrop=true, bottom=%d !"
         "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
         "videoconvert ! "
         "appsink"
-        % (
+        % ( 0
             capture_width,
             capture_height,
             framerate,
