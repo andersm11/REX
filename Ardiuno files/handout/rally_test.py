540c7c4e4b0d4ae7ceacc8d9a4d32b8606064c2e
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-11-02 11:36:22 +0100

    Update rally_test.py

--- Ardiuno files/handout/rally_test.py
+++ Ardiuno files/handout/rally_test.py
@@ -226,11 +226,11 @@ try:
                     print(target_object.angle)
                     if target_object.angle > 11:
                         arlo.go_diff(30,30,0,1)
-                        sleep(0.0153*target_object.angle)
+                        sleep(0.0153*abs(target_object.angle))
                         arlo.stop()
                     elif target_object.angle < -11:
                         arlo.go_diff(30,30,1,0)
-                        sleep(0.0153*target_object.angle)
+                        sleep(0.0153*abs(target_object.angle))
                         arlo.stop()
         else:
             target_object = None
