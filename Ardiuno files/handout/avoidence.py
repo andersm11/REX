c951a639249c29966df8ace4ffdcd89126630ca8
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-11-03 14:21:15 +0100

    Update avoidence.py

--- Ardiuno files/handout/avoidence.py
+++ Ardiuno files/handout/avoidence.py
@@ -413,30 +413,33 @@ def main():
             sleep(0.5)
             arlo.stop()
         elif state == 0 and counter >= 13 and ids is not None:
+            arlo.go_diff(30,30,0,1)
+            sleep(0.5)
             t_corners, t_id = check_id_mod(corners,ids,last_orientation_box)
             rvec, tvec, objPoints = cv2.aruco.estimatePoseSingleMarkers(t_corners,15,cam_intrinsic_matrix,cam_distortion_coeffs)
-            angle, distance = compute_angle_and_distance(tvec)
-            print("angle",angle)
-            print("dist:", distance)
-            turn(angle)
-            sleep(0.5)
-            if distance > 300:
-                time_to_drive = 0.028*(abs(distance-10)/2)
-            else:
-                time_to_drive = 0.028*(abs(distance-10))
-            print("Going forward")
-            last_orientation_box = t_id
-            robot_drive(1)
-            start_time = time.time()
-            end_time = start_time
-            check = "free"
-            while check == "free":
-                print("i am moving")
-                end_time = time.time()
-                check = avoidance()
-                if end_time-start_time >= time_to_drive:
-                    break
-            arlo.stop()
+            if tvec is not None:
+                angle, distance = compute_angle_and_distance(tvec)
+                print("angle",angle)
+                print("dist:", distance)
+                turn(angle)
+                sleep(0.5)
+                if distance > 300:
+                    time_to_drive = 0.028*(abs(distance-10)/2)
+                else:
+                    time_to_drive = 0.028*(abs(distance-10))
+                print("Going forward")
+                last_orientation_box = t_id
+                robot_drive(1)
+                start_time = time.time()
+                end_time = start_time
+                check = "free"
+                while check == "free":
+                    print("i am moving")
+                    end_time = time.time()
+                    check = avoidance()
+                    if end_time-start_time >= time_to_drive:
+                        break
+                arlo.stop()
 
 
     
