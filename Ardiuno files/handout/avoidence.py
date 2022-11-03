5336c437e82f12e0127552bb139a64a4877c86f7
andersm11 <79901566+andersm11@users.noreply.github.com>
2022-11-03 11:41:26 +0100

    Update avoidence.py

--- Ardiuno files/handout/avoidence.py
+++ Ardiuno files/handout/avoidence.py
@@ -357,7 +357,7 @@ def main():
             turn(angle)
             sleep(1)
             start_time = time.time()
-            time_to_drive = 0.028*(abs(distance-10))
+            time_to_drive = 0.028*(abs(distance-8))
             state = 1
             
         if state == 1:
