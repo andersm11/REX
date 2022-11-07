a3db125bea1389544ecb54e05f19016bdf4fa5f2
qhk895 <79901178+qhk895@users.noreply.github.com>
2022-11-07 10:28:26 +0100

    Update rally.py

--- Ardiuno files/handout/rally.py
+++ Ardiuno files/handout/rally.py
@@ -465,7 +465,7 @@ def main():
                             arlo.go_diff(45,30,1,0)
                             turn(100)
                             robot_drive(1)
-                            sleep(1)
+                            sleep(1.5)
                             arlo.stop()
                             search_side = "s_left"
                         counter = 0
